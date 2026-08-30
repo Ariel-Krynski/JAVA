ff012e309e1367d58ba8df8efbfda166f093e356
Ariel-Krynski <arielkrynski@gmail.com>
2026-08-30 03:41:34 -0300

    MODIFIQUE EL CODIGO DE ORDENAR POR BURBUJA  Y LO COMENTE
    He agregado 2, el útil es OrdenarPorBurbuja. OrdenarUnArregloPorBurbuja es un codigo donde experimente el cambiar los roles de los bucles y fui jugando con el código.

--- src/OrdenarPorBurbuja.java
+++ src/OrdenarPorBurbuja.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
+//NOTA, EL PROFESOR USO "temp", como var auxiliar. Yo he cambiado ese termino por "aux", por costumbre  
 
-// Arrays.toString() y mostrar el contenido del arreglo.
+// Arrays.toString() MUESTRA EL CONTENIDO DEL ARREGLO
 
 public class OrdenarPorBurbuja {
 
@@ -9,51 +10,46 @@ public class OrdenarPorBurbuja {
         int[] matriz = {9, 3, 8, 4, 6, 7, 2, 3};
 
         for (int n = 1; n < matriz.length; n++) {
-            // Controla la cantidad de pasadas que se realizan sobre el arreglo.
-            // n comienza en 1 y llega hasta matriz.length - 1.
+            // Controla la cantidad de pasadas que se realizan sobre el arreglo.. Imaginemoslo como un lapiz arrastrandose
+        
 
             for (int i = 0; i < matriz.length - n; i++) {
-                // Bucle interno.
-                // Compara elementos consecutivos del arreglo.
-                // Se resta n porque en cada pasada el elemento más grande ya queda colocado al final.
+                // Compara elementos consecutivos
+                // Se resta n porque en cada pasada el elemento más grande ya queda colocado al final
 
-                // DEBUG //
                 System.out.println("Posición que se esta analizando: "+ i);
-                // Muestra la posición que se está analizando.
+                // Muestra la posición que se está analizando
 
                 System.out.println(Arrays.toString( matriz));
-                // Muestra cómo está el arreglo antes de realizar
-                // la comparación.
+                // Muestra cómo está el arreglo antes de realizar la comparación de elementos
 
                 System.out.println((matriz[i]) + " - " + (matriz[i + 1]));
-                // Muestra los dos elementos que se van a comparar.
+                // Muestra los dos elementos que se van a comparar
 
-                // DEBUG //
 
                 if (matriz[i] > matriz[i + 1]) {
                     // Si el elemento actual es mayor que el siguiente,
-                    // significa que están en el orden incorrecto
-                    // y hay que intercambiarlos.
+                    // significa que están en el orden incorrecto y hay que intercambiarlos.
 
-                    int temp = matriz[i + 1];
+                    int aux = matriz[i + 1];
                     // Guardamos temporalmente el segundo elemento.
 
                     matriz[i + 1] = matriz[i];
                     // Colocamos el elemento mayor en la posición siguiente.
 
-                    matriz[i] = temp;
+                    matriz[i] = aux;
                     // Colocamos el elemento que habíamos guardado
                     // en la posición actual.
                 }
 
                 System.out.println(Arrays.toString(matriz));
-                // Mostramos el arreglo después de la comparación
-                // y del posible intercambio.
+                // Mostramos el arreglo después de la comparacióN (PUEDE O NO HABER INTERCAMBIO)
+
             }
         }
 
         System.out.println(Arrays.toString(matriz));
         // Cuando terminan todos los ciclos, mostramos el arreglo
-        // completamente ordenado.
+        
     }
 }
